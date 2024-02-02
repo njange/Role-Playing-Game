51b4f658ac1b6cdc67c09afd20c307639bb59394
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-03 02:50:56 +0300

    go store function added

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+  }

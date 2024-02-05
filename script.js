45089acf57394991ffbb1932d9f3233ce7d6b1ba
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-05 23:54:58 +0300

    locations updated code

--- script.js
+++ script.js
@@ -17,6 +17,14 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"]
+  }
+];
+
+
 // initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;

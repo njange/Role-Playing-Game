7d1e819851ac95d069f35ae02a97dcf25479dd3d
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-05 23:26:58 +0300

    updated go town function

--- script.js
+++ script.js
@@ -32,29 +32,30 @@ function goTown() {
   text.innerText = "You enter the store.";
 }
 
-function goStore() {
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-    text.innerText = "You enter the store.";
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says Store.";
 }
 
 
-  function goCave() {
+function goCave() {
     console.log("Going to cave.");
   }
   
-  function fightDragon() {
+function fightDragon() {
     console.log("Fighting dragon.");
   }
 
-  function buyHealth() {
+ function buyHealth() {
 
   }
   
-  function buyWeapon() {
-  
+function buyWeapon() {
+
   }
+

4f8ba20b78bb9bf171b88eb6300be1108dfd4a4b
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-07 03:09:56 +0300

    weapons function

--- script.js
+++ script.js
@@ -90,7 +90,13 @@ function fightDragon() {
   }
   
 function buyWeapon() {
-
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeapon].name;
+    text.innerText = "You now have a new weapon.";
+  }
   }
 
   function fightSlime(){

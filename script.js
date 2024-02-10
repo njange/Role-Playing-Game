905552dda2b0a8df56098598c163765ce2d2bb04
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-02-11 02:15:21 +0300

    monster health

--- script.js
+++ script.js
@@ -159,5 +159,7 @@ function buyWeapon() {
   function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
-    
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
   }
